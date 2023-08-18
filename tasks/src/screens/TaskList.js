e7961eeeef1316545e4be32c598b686b8c92825b
Lucas <lucasnardellisaar@gmail.com>
2023-08-18 20:21:29 -0300

    Inserindo data na criação de noca tarefa

--- tasks/src/screens/TaskList.js
+++ tasks/src/screens/TaskList.js
@@ -7,6 +7,8 @@ import todayImage from '../../assets/imgs/today.jpg'
 import Icon from 'react-native-vector-icons/FontAwesome.js'
 
 import Task from '../components/Task.js'
+import AddTask from "./AddTask.js"
+
 import moment from 'moment'
 import 'moment/locale/pt-br'
 
@@ -15,6 +17,7 @@ export default class TaskList extends Component {
 
     state = {
         showDoneTasks: true,
+        showAddTask: false,
         visibleTasks: [],
         tasks: [{
             id: Math.random(),
@@ -64,6 +67,8 @@ export default class TaskList extends Component {
         const today = moment().locale('pt-br').format('ddd, D [de] MMMM [de] YYYY')
         return(
             <SafeAreaView style={styles.container}>
+                <AddTask isVisible={this.state.showAddTask} 
+                onCancel={() => this.setState({ showAddTask: false })}/>
                 <ImageBackground source={todayImage}
                     style={styles.background}>
                     <View style={styles.iconBar}>
@@ -83,6 +88,11 @@ export default class TaskList extends Component {
                     />
 
                 </View>
+                <TouchableOpacity style={styles.addButton}
+                    onPress={() => this.setState({ showAddTask: true })}
+                    activeOpacity={0.7}>
+                    <Icon name="plus" size={20} color={commonStyles.colors.secondary}/>
+                </TouchableOpacity>
                 
             </SafeAreaView>
         )
@@ -123,5 +133,16 @@ const styles = StyleSheet.create({
         marginHorizontal: 20,
         justifyContent: 'flex-end',
         marginTop: Platform.OS === 'ios' ? 40 : 10
+    },
+    addButton: {
+        position: 'absolute',
+        right: 30,
+        bottom: 30,
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        backgroundColor: commonStyles.colors.today,
+        justifyContent: 'center',
+        alignItems: 'center',
     }
 })
