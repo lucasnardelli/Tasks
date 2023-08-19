4674c450d4d449f474b44ce55b1dda4cbc2a35ed
Lucas <lucasnardellisaar@gmail.com>
2023-08-19 15:09:44 -0300

    Fazendo o backend da aplicação

---
+++ tasks-backend/api/auth.js
@@ -0,0 +1,33 @@
+const { authSecret } = require('../.env')
+const jwt = require('jwt-simple')
+const bcrypt = require ('bcrypt-nodejs')
+
+module.exports = app => {
+    const singin = async (req, res) => {
+        if (!req.body.email || !req.body.password) {
+            return res.status(400).send('Dados incompletos')
+        }
+
+        const user = await app.db('users')
+            .where({ email: req.body.email })
+            .first()
+
+        if (user) {
+            bcrypt.compare(re.body.password, user.password, (err, isMatch) => {
+                if (err || !isMatch){
+                    return res.status(401).send('Senha inválida')
+                }
+
+                const payload = { id: user.id }
+                res.json({
+                    nome: user.name,
+                    email: user.email,
+                    token: jwt.encode(payload, authSecret),
+                })
+            })
+        } else {
+            res.status(400).send('Email não cadastrado')
+        }
+    }
+    return { singin }
+}
---
+++ tasks-backend/config/routes.js
@@ -0,0 +1,17 @@
+module.exports = app => {
+    app.post('/singup', app.api.user.save)
+    app.post('/singin', app.api.auth.singin)
+
+    app.route('/tasks')
+        .all(app.config.passport.authenticate())
+        .get(app.api.task.getTasks)
+        .post(app.api.task.save)
+
+    app.route('/tasks/:id')
+        .all(app.config.passport.authenticate())
+        .delete(app.api.task.remove)
+
+    app.route('/tasks/:id/toggle')
+        .all(app.config.passport.authenticate())
+        .put(app.api.task.toggleTask)
+}   
